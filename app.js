8b6b287740c35ecc460fa6b6327b97257d732818
DeanBolte <dean.bolte7@gmail.com>
2021-09-17 23:14:18 +1000

    modified play method layout

--- app.js
+++ app.js
@@ -139,13 +139,6 @@ function play(guild, song) {
   // get queue
   const songQueue = queue.get(guild.id);
 
-  // leave if there is no song to play
-  if (!song) {
-    songQueue.connection.destroy();
-    queue.delete(guild.id);
-    return;
-  }
-
   // create resources for music player
   const stream = ytdl(song.url, { filter: 'audioonly' });
   const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
@@ -159,7 +152,13 @@ function play(guild, song) {
   // on music ending
   player.on(AudioPlayerStatus.Idle, () => {
     songQueue.songs.shift();
-    play(guild, songQueue.songs[0]);
+    if(songQueue.songs[0]) {
+      play(guild, songQueue.songs[0]);
+    } else {
+      // leave if there is no song to play
+      songQueue.connection.destroy();
+      queue.delete(guild.id);
+    }
   });
 }
 
